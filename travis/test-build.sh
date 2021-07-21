d8a36cc5be2f225d7edd7a236f40a3a1e019e81a
Chris Li <chrli@chrli-mn1.linkedin.biz>
2021-07-21 16:43:54 -0700

    onboard to JFrog

--- travis/test-build.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 LinkedIn Corporation. All rights reserved.
-# Licensed under the BSD-2 Clause license.
-# See LICENSE in the project root for license information.
-
-#!/bin/bash
-set -e
-
-echo "Starting $0 at " $(date)
-time ./gradlew clean build -x test -x javadoc -Dorg.gradle.parallel=true $DIL_GRADLE_OPTS
