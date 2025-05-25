57cc407cdd4b2a3d7b2d9b0e7180f735a09be7fc
Shanu Gautam <shagauta@shagauta-mn7596.linkedin.biz>
2025-05-25 21:59:46 +0530

    removing comment from enum class

--- cdi-core/src/main/java/com/linkedin/cdi/util/JsonElementTypes.java
+++ cdi-core/src/main/java/com/linkedin/cdi/util/JsonElementTypes.java
@@ -44,7 +44,7 @@ public enum JsonElementTypes {
   INTEGER("integer", "int"),
   INT64("int64", "long"),
   LONG("long"),
-  MAP("map", "map", false), // This is for nested json support
+  MAP("map", "map", false),
   NUMBER("number", "double"),
   OBJECT("object", "record", false),
   PRIMITIVE("primitive"),
